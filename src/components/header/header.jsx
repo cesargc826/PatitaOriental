d9c4d2bfa2df21fb3ca6de1da3aefc7c30ad2563
Francisco Caldera <a19110098@ceti.mx>
2025-05-30 00:01:53 -0600

    feat: Se acomoda header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,44 +4,47 @@ import "./header.css";
 const Header = () => {
   return (
     <header>
-      <div className="header-container navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
+      <div >
+        <div className="container-fluid header-container">
         {/* <!-- Logo a la izquierda --> */}
-        <a href="index.html" className="logo navbar-band">
-          <img
+        <a href="index.html" className="logo navbar-brand">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+           </button>
+          <img id="logo-header"
             srcSet="./images/PatitaOriental_Azul_Horizontal.png"
-            alt="Logo Patita Oriental"
+            alt="Logo Patita Oriental" 
           ></img>
         </a>
 
         {/* <!-- Menú al centro --> */}
-        <nav className="collapse navbar-collapse">
-          <ul className="navegacion navbar-nav">
-            <li className="nav-item">
+        <nav>
+          <ul>
+            <li className="nav-item navegacion">
               <a href="#">Menú</a>
             </li >
-            <li className="nav-item">
+            <li className="nav-item navegacion">
               <a href="#">Menú Temático</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item navegacion">
               <a href="#">Contacto</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item navegacion">
               <a href="#">Sobre Nosotros</a>
             </li>
           </ul>
         </nav>
 
         {/* <!-- Iconos a la derecha --> */}
-        <div className="iconos container-fluid" >
+        <div>
           <a href="#">
-            <img srcSet="./images/LogoUsr.jpeg" alt="Perfil"></img>
+            <img srcSet="./images/LogoUsr.jpeg" alt="Perfil" class="iconos nav-item"></img>
           </a>
           <a href="#">
-            <img srcSet="./images/LogoLove.jpeg" alt="Favoritos"></img>
+            <img srcSet="./images/LogoLove.jpeg" alt="Favoritos" class="iconos nav-item"></img>
           </a>
           <a href="#">
-            <img srcSet="./images/LogoCart.jpeg" alt="Carrito"></img>
+            <img srcSet="./images/LogoCart.jpeg" alt="Carrito" class="iconos nav-item"></img>
           </a>
         </div>
       </div>
