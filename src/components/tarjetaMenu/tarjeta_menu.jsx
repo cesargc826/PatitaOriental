2c97268ee5af3f77254029b71385cc767d37951a
Abigail Galvez <galvezabigail79@gamil.com>
2025-05-30 16:14:12 -0600

    tarjeta menú con json

--- src/components/tarjetaMenu/tarjeta_menu.jsx
+++ src/components/tarjetaMenu/tarjeta_menu.jsx
@@ -6,13 +6,11 @@ const TarjetaMenu = () => {
              <br></br>
               <br></br>
             <div className="contenedor-producto">
-                <img srcSet="../../public/images/imagen-menu-1.png" alt="imagen de comida" />
+                <img src={imagen} alt={`imagen de ${nombre}`} />
                 <div className="contenedor-info-producto">
-                    <h1 className="titulo-comida">DUMPLINGS</h1>
-                    <h3 className="precio-comida">$300.00</h3>
-                    <p className="descripcion-comida">
-                        Diversidad y sabor, con platos que van desde guisos y carnes a la parrilla hasta fideos y dulces.
-                    </p>
+                    <h1 className="titulo-comida">{nombre}</h1>
+                    <h3 className="precio-comida">${precio}.00</h3>
+                    <p className="descripcion-comida">{descripcion}</p>
                 </div>
             </div>
             <div className="contenedor-botones-menu">
