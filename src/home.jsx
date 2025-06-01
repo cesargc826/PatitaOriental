17b25d9457cf39dfbca45730899eb4910d5ba338
Gabriel Alvarez <jgabriel96@hotmail.com>
2025-06-01 11:05:57 -0600

    Modified: Se modifico home para que solo se utilizara para la pagina principal

--- src/home.jsx
+++ src/home.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import TarjetaMenu from "./components/tarjetaMenu/tarjeta_menu.jsx";
-import Footer from "./components/footer/footer.jsx";
-import "./home.css";
-import Header from "./components/header/header.jsx";
-
 
 function Home() {
-  return (
-    <div>
-      <Header />
-      
-      <Footer />
-    </div>
+  return(
+      <h1> Home</h1>
   );
 }
 
